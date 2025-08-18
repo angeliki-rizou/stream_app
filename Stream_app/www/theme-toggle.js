49a5892647ac23b16087dd7ea63cf5be381f257d
Μαρία Κούρτη <92392853+mariakourti@users.noreply.github.com>
2025-08-18 19:43:18 +0300

    Update theme-toggle.js

--- Stream_app/www/theme-toggle.js
+++ Stream_app/www/theme-toggle.js
@@ -1,4 +1,14 @@
+function theme_toggle() {
+    var element = document.body;
+    element.classList.toggle("theme-toggle");
+
 let element = document.body;
-let is_darkmode_enable = element.classList.toggle("dark-mode");
+let is_darkmode_enable = element.classList.toggle("theme-toggle");
 document.cookie = "darkmode_enable=" + is_darkmode_enable;
 localStorage.setItem("darkmode_enable", is_darkmode_enable);
+
+let darkmode_default = localStorage.getItem("darkmode_enable") || false;
+let element = document.body;
+element.classList.toggle("theme-toggle", darkmode_default);
+
+}
