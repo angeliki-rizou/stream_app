12587c88c0db74b001f136f29c2664098eaa59ea
angeliki-rizou <soaggeliki@hotmail.com>
2025-08-18 19:56:14 +0300

    Update theme-toggle.js

--- Stream_app/www/theme-toggle.js
+++ Stream_app/www/theme-toggle.js
@@ -1,14 +1,15 @@
 function theme_toggle() {
-    var element = document.body;
-    element.classList.toggle("theme-toggle");
+    let element = document.body;
+    let is_darkmode_enable = element.classList.toggle("theme-toggle");
 
-let element = document.body;
-let is_darkmode_enable = element.classList.toggle("theme-toggle");
-document.cookie = "darkmode_enable=" + is_darkmode_enable;
-localStorage.setItem("darkmode_enable", is_darkmode_enable);
-
-let darkmode_default = localStorage.getItem("darkmode_enable") || false;
-let element = document.body;
-element.classList.toggle("theme-toggle", darkmode_default);
-
+    document.cookie = "darkmode_enable=" + is_darkmode_enable;
+    localStorage.setItem("darkmode_enable", is_darkmode_enable);
 }
+
+window.addEventListener("load", function () {
+    let darkmode_default = localStorage.getItem("darkmode_enable") === "true";
+    if (darkmode_default) {
+        document.body.classList.add("theme-toggle");
+    }
+});
+
