56762ea6faaec9a7e4978b620abeb6ebe0c4bb53
Μαρία Κούρτη <92392853+mariakourti@users.noreply.github.com>
2025-08-19 15:25:49 +0300

    Add files via upload

---
+++ Stream_app/www/intro.js
@@ -0,0 +1,17 @@
+function intro(event, Εισαγωγή) {
+
+  var i, tabcontent, tablinks;
+
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  document.getElementById(Εισαγωγή).style.display = "block";
+  evt.currentTarget.className += " active";
+} 
