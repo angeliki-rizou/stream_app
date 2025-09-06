f6022390ebad6a7390d3351f79e56bec48617670
angeliki-rizou <soaggeliki@hotmail.com>
2025-09-06 17:29:45 +0300

    Update followers.php

--- Stream_app/www/followers.php
+++ Stream_app/www/followers.php
@@ -18,7 +18,7 @@ try {
         SELECT u.id, u.username, u.first_name, u.last_name 
         FROM followers f
         JOIN users u ON f.follower_id = u.id
-        WHERE f.follower_id = ?
+        WHERE f.following_id = ?
     ");
     $stmt->execute([$_SESSION['user_id']]);
     $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -26,30 +26,58 @@ try {
 } catch (PDOException $e) {
     die("Σφάλμα: " . $e->getMessage());
 }
+
+// τίτλος σελίδας
+$page_title = "Οι ακόλουθοί μου ";
+
+
+ob_start();
 ?>
-<!doctype html>
-<html lang="el">
-<head>
-<meta charset="utf-8">
-<title>Οι ακόλουθοί μου</title>
-</head>
-<body>
-<h1>👥 Οι ακόλουθοί μου</h1>
-<a href="protected.php">⬅ Επιστροφή στο μενού</a>
+<h1> Οι ακόλουθοί μου</h1>
+<a href="protected.php" class="w3-button w3-blue">⬅ Επιστροφή στο μενού</a>
 <hr>
 
 <?php if ($users): ?>
-    <ul>
-    <?php foreach ($users as $user): ?>
-        <li>
-            <?php echo htmlspecialchars($user['username']); ?> 
-            (<?php echo htmlspecialchars($user['first_name'] . " " . $user['last_name']); ?>)
-        </li>
-    <?php endforeach; ?>
-    </ul>
+    <div class="w3-container">
+        <div class="w3-panel w3-blue w3-round">
+            <h3>Συνολικοί ακόλουθοι: <?php echo count($users); ?></h3>
+        </div>
+        
+        <div class="w3-row-padding">
+            <?php foreach ($users as $user): ?>
+            <div class="w3-col s12 m6 l4 w3-margin-bottom">
+                <div class="w3-card w3-padding w3-hover-shadow">
+                    <div class="w3-center">
+                        <div class="w3-circle w3-blue w3-padding" style="width: 60px; height: 60px; line-height: 60px;">
+                            <i class="fa fa-user" style="font-size: 24px;"></i>
+                        </div>
+                    </div>
+                    <h3 class="w3-center"><?php echo htmlspecialchars($user['username']); ?></h3>
+                    <p class="w3-center w3-text-gray"><?php echo htmlspecialchars($user['first_name'] . " " . $user['last_name']); ?></p>
+                    <div class="w3-center">
+                        <a href="view_user.php?id=<?php echo $user['id']; ?>" class="w3-button w3-blue w3-small w3-round">👁️ Προβολή προφίλ</a>
+                    </div>
+                </div>
+            </div>
+            <?php endforeach; ?>
+        </div>
+    </div>
 <?php else: ?>
-    <p>Δεν σε ακολουθεί κανένας ακόμα.</p>
+    <div class="w3-container">
+        <div class="w3-panel w3-yellow w3-round">
+            <h3>Δεν σε ακολουθεί κανένας ακόμα</h3>
+            <p>Μοιραστείτε το προφίλ σας με άλλους για να αρχίσουν να σας ακολουθούν!</p>
+        </div>
+        <div class="w3-center">
+            <a href="search_users.php" class="w3-button w3-blue w3-round"> Αναζήτηση χρηστών</a>
+            <a href="profile.php" class="w3-button w3-green w3-round"> Επεξεργασία προφίλ</a>
+        </div>
+    </div>
 <?php endif; ?>
 
-</body>
-</html>
+<?php
+$content = ob_get_clean();
+
+// layout
+include("layout.php");
+?>
